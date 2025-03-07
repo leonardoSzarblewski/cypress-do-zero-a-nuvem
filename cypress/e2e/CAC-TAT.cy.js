747af8cf3a35aebeeafca488b0e754b40fac9200
Leonardo <leonardo27.laurindo@gmail.com>
2025-03-07 14:10:45 -0300

    criando uma constante para textos longos

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -3,7 +3,6 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.visit('./src/index.html')
 
   })
-
   it('verifica o titulo da aplicação', () => {
 
     cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
@@ -11,11 +10,12 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
 
   it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('leo', 20)
 
     cy.get('#firstName').type('leonardo')
     cy.get('#lastName').type('laurindo')
     cy.get('#email').type('leo@gmail.com')
-    cy.get('#open-text-area').type('testando teste 123....@@@')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
     cy.get('button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
