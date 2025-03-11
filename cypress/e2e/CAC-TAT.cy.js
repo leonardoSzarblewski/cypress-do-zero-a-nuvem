48be354abdef420472dc1d01374c10c18e7d5578
Leonardo <leonardo27.laurindo@gmail.com>
2025-03-11 12:07:37 -0300

    Criando comando customizado para preencher e limpar input

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -15,14 +15,14 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('laurindo')
     cy.get('#email').type('leo@gmail.com')
     cy.get('#open-text-area').type(longText, { delay: 0 })
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.success').should('be.visible')
   })
 
   it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida',() => {
     cy.get('#email').type('test')
-    cy.get('.button').click()    
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -41,42 +41,23 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#email').type('leo@gmail.com')
     cy.get('#phone-checkbox').click()
     cy.get('#open-text-area').type(longText, { delay: 0 })
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
 
-  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
-    cy.get('#firstName')
-      .type('leonardo')
-      .should('have.value', 'leonardo')
-      .clear()
-      .should('have.value', '')
-    cy.get('#lastName')
-      .type('laurindo')
-      .should('have.value', 'laurindo')
-      .clear()
-      .should('have.value', '')
-    cy.get('#email')
-      .type('leo@gmail.com')
-      .should('have.value', 'leo@gmail.com')
-      .clear()
-      .should('have.value', '')
-    cy.get('#phone')
-      .type('999999999')
-      .should('have.value', '999999999')
-      .clear()
-      .should('have.value', '')
+  it.only('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.fillAndCleanInputs()
   })
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
     cy.get('.error').should('be.visible')
 
     
   })
 
-  it.only('envia o formuário com sucesso usando um comando customizado', () => {
+  it('envia o formuário com sucesso usando um comando customizado', () => {
     cy.fillMandatoryFieldsAndSubmit()
 
     cy.get('.success').should('be.visible')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,11 +4,33 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', ( data = {
     email: 'test@gmail.com',
     text: 'ok testando 123'
 }) => {
-    const longText = Cypress._.repeat('leo', 20)
 
     cy.get('#firstName').type(data.firstName)
     cy.get('#lastName').type(data.lastName)
     cy.get('#email').type(data.email)
     cy.get('#open-text-area').type(data.text)
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
+})
+
+Cypress.Commands.add('fillAndCleanInputs', () => {
+    cy.get('#firstName')
+      .type('leonardo')
+      .should('have.value', 'leonardo')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('laurindo')
+      .should('have.value', 'laurindo')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('leo@gmail.com')
+      .should('have.value', 'leo@gmail.com')
+      .clear()
+      .should('have.value', '')
+    cy.get('#phone')
+      .type('999999999')
+      .should('have.value', '999999999')
+      .clear()
+      .should('have.value', '')
 })
