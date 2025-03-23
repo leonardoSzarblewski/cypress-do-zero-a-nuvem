f2a88f503c4ba1b92b914bf47eab70845189e434
Leonardo <leonardo27.laurindo@gmail.com>
2025-03-23 00:16:52 -0300

    fazendo um teste

--- src/script.js
+++ src/script.js
@@ -29,13 +29,11 @@ document.querySelector('button[type="submit"]')
     const successMessage = document.querySelector('.success')
     if (!firstNameField.value || !lastNameField.value || !emailField.value || !textareaField.value) {
       return showAndHideErrorMessage()
-    }
+    } 
     if (isPhoneRequired && !phoneField.value) {
       return showAndHideErrorMessage()
     }
-    if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-       return showAndHideErrorMessage()
-    }
+    
     firstNameField.value = ''
     lastNameField.value = ''
     emailField.value = ''
